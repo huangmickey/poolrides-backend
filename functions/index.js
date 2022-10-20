add1c543bc836016d93ec705b0255780331f2e67
huangmickey <916m.huang@gmail.com>
2022-10-20 15:02:26 -0700

    fixing structure and adding in updaed index.js

--- functions/index.js
+++ functions/index.js
@@ -3,10 +3,6 @@ const functions = require("firebase-functions")
 const admin = require('firebase-admin')
 admin.initializeApp()
 
-// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
-// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
-// `Authorization: Bearer <Firebase ID Token>`.
-// when decoded successfully, the ID Token content will be added as `req.user`.
 const validateFirebaseIdToken = async (req, res, next) => {
   if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer '))) {
     functions.logger.error(
@@ -21,10 +17,8 @@ const validateFirebaseIdToken = async (req, res, next) => {
 
   let idToken;
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
-    // Read the ID Token from the Authorization header.
     idToken = req.headers.authorization.split('Bearer ')[1]
   } else {
-    // No cookie
     res.status(403).send('Unauthorized')
     return;
   }
