67ef05a4c2eaf0eadc53c9c165184ddce6fb92d6
SomeOrdinaryGamer <81064130+SomeOrdinaryGamer@users.noreply.github.com>
2022-10-20 15:10:12 -0700

    Built out more of the backend
    -Added function to get all the activeDrivers.
    -Added function to narrow down lost to drivers within 10 miles
    -Added selection algorithm to select closest driver

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,17 @@ const functions = require("firebase-functions")
 const admin = require('firebase-admin')
 admin.initializeApp()
 
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
 const validateFirebaseIdToken = async (req, res, next) => {
   if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer '))) {
     functions.logger.error(
+      'No Firebase ID token was passed as a Bearer token in the Authorization header.',
+      'Make sure you authorize your request by providing the following HTTP header:',
+      'Authorization: Bearer <Firebase ID Token>',
+      'or by passing a "__session" cookie.'
     );
     res.status(403).send('Unauthorized')
     return;
@@ -13,8 +21,10 @@ const validateFirebaseIdToken = async (req, res, next) => {
 
   let idToken;
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    // Read the ID Token from the Authorization header.
     idToken = req.headers.authorization.split('Bearer ')[1]
   } else {
+    // No cookie
     res.status(403).send('Unauthorized')
     return;
   }
@@ -65,98 +75,108 @@ exports.postmethod = functions.https.onRequest(async (request, res) => {
         res.status(400).send('Incorrect Payload')
       }
 
-      //NOTES FOR MICKEY.
-      //1. If it costs money to write to a collection then we dont need to write each request to the DB
-      //2. Should we get ask for a response from the rider when we alert then that they have a driver? or should we
-      //just end it there?
-      //3. Note, we are currently not using isProccessed. and I think isAccepted is used later.
-      //4. Alot of the commented out stuff is there for future, I just wrote it out to give me an idea of the workflow
-      // Getting the Active Driver list should work though from my testing
+      var driverList = await getActiveDriverList();
 
-      //I would like to turn the code below into a function in its own but I havent gotten around to changing it
-      //As it stands though, it should return anything inside of the activeDrivers collection.
-      var driverList;
-      admin.firestore().collection("activeDrivers").get()
-        .then(snapshot => {
-          driverList = snapshot.docs.map(doc => {
-            var driverID = doc.id;
-            var obj2 = doc.data();
-            var object = {
-              driverID,
-              ...obj2
-            };
-            return object;
-          });
-          console.log("Here is a list of current Drivers." + driverList);
-          return driverList;
-        }).catch(function (error) {
-          res.status(400).send('Error retrieving Drivers')
-          return false;
-        })
-
-      // Here I have started to move the driver list to a function but haven't gotten it to work 
-      // var driverList = await getActiveDriverList();   //returns list of drivers within  of driver coordinates (driverID) 
-      console.log("this is the driver list" + driverList)
+      /**/console.log("======================================================================")
+      /**/console.log("Results for the Active Driver List:\n")
+      /**/driverList.forEach(driver => {
+        /**/console.log("Driver ID: " + driver.DriverID)
+        /**/console.log("Driver Push Token: " + driver.PushToken)
+        /**/console.log("Driver Lat: " + driver.lat)
+        /**/console.log("Driver Lng: " + driver.lng)
+        /**/console.log("\n")
+      /**/});
+      /**/console.log(driverList.length)
+      /**/console.log("======================================================================")
+      /**/console.log("\n")
 
       if (driverList.length == 0 || driverList == false) {
-        // If list length=0. No drivers. Return false
         res.status(400).send("No Drivers On App. Sorry :(");
         return false;
       } else {
-        //Else. There are drivers. Now remove those who are to far
-        //In this function we will loop over the array and use Haversine to compare distances. we will return a new list with drivers who are close
-        //var newDriverList = removeDrivers(driverList); 
+        var newDriverList = removeDrivers(driverList, body.originLat, body.originLng);
 
+        /**/console.log("======================================================================")
+        /**/console.log("Results for the New Shortened Driver List:\n")
+        /**/newDriverList.forEach(driver => {
+          /**/console.log("Driver ID: " + driver.DriverID)
+          /**/console.log("Driver Push Token: " + driver.PushToken)
+          /**/console.log("Driver Lat: " + driver.lat)
+          /**/console.log("Driver Lng: " + driver.lng)
+          /**/console.log("\n")
+        /**/});
+        /**/console.log(newDriverList.length)
+        /**/console.log("======================================================================")
+        /**/console.log("\n")
+        
         if (newDriverList.length == 0 || newDriverList == false) {
-          //No Drivers Nearby. Return false. Its key to recheck length as we might not have any nearby
           res.status(400).send("No Drivers nearby. Sorry :(");
           return false;
         } else {
-          //Start comparing interests
-          var best5Drivers = compareInterests(userID, newDriverList);
-          //We can either pass the userdata in payload but it might be easier to just call DB amnd get user Interests
-
+          var bestDriver = compareInterests(body.userID, newDriverList, body.originLat, body.originLng);
+          /**/console.log("======================================================================")
+          /**/console.log("THE BEST DRIVER SELECTED ISSSSS:\n")
+          /**/console.log("Driver ID: " + bestDriver.DriverID)
+          /**/console.log("Driver Push Token: " + bestDriver.PushToken)
+          /**/console.log("Driver Lat: " + bestDriver.lat)
+          /**/console.log("Driver Lng: " + bestDriver.lng)
+          /**/console.log("======================================================================")
+          /**/console.log("\n")
+          
+          // var isAccepted = false;
           // while(!isAccepted || noDriversAccept) {
-          // Now start sending FCM messages to each driver. and wait for them to return response or set a timeout 
-          // and if the driver doesnt respond then move to the next. If none respond then set a var noDriversAccept = true
+          // // Now start sending FCM messages to each driver. and wait for them to return response or set a timeout 
+          // // and if the driver doesnt respond then move to the next. If none respond then set a var noDriversAccept = true
 
-          // If Driver accepts
+          // // If Driver accepts
           // isAccepted = true
           // }
 
-          //Alert the user that they have a ride or in the case that no drivers responded, respond that you couldnt find a driver
-          //sendUserFCM();
+          // //Alert the user that they have a ride or in the case that no drivers responded, respond that you couldnt find a driver
+          // sendUserFCM();
 
           res.status(200).send("This is a response from the server that your request has been ackowledged." + request.body)
           return true;
-        } //end of else for newDriverList.Length == 0
-      } // end of else for driverList.Length ==0
+        }
+
+
+        res.status(200).send("This is a response from the server that your request has been ackowledged." + request.body)
+        return true;
+
+      } 
     } else {
       res.status(401).send('You are not authorized')
       return false
-    } // end of else for ifValidUser
+    }
   }
 });
 
 
 async function getActiveDriverList() {
-  admin.firestore().collection("activeDrivers").get()
-    .then(snapshot => {
-      let driverList = snapshot.docs.map(doc => {
-        var driverID = doc.id;
-        var obj2 = doc.data();
-        var object = {
-          driverID,
-          ...obj2
-        };
-        return object;
-      });
-      console.log("Here is a list of current Drivers." + driverList);
-      return driverList;
-    }).catch(function (error) {
-      res.status(400).send('Error retrieving Drivers')
-      return false;
-    })
+  try {
+    let collection = await admin.firestore().collection("activeDrivers").get()
+    return (collection.docs.map((doc) => (doc.data())));
+  } catch {
+    return false;
+  }
+}
+
+function removeDrivers(driverList, originLat, originLng) {
+  let newDriverList = [];
+  driverList.forEach(driver => {
+
+    /**/console.log("Driver ID: " + driver.DriverID)
+    /**/console.log("Driver Lat: " + driver.lat)
+    /**/console.log("Driver Lng: " + driver.lng)
+    /**/console.log("Origin Lat: " + originLat)
+    /**/console.log("Origin Lng: " + originLng)
+    /**/console.log("Distance between 2 points: " + distanceMatrix(driver.lat, driver.lng, originLat, originLng) + "\n")
+
+    if (distanceMatrix(driver.lat, driver.lng, originLat, originLng) < 10) {
+      newDriverList.push(driver);
+    }
+  });
+  return newDriverList
 }
 
 function distanceMatrix(lat1, lon1, lat2, lon2) {
@@ -177,3 +197,38 @@ function distanceMatrix(lat1, lon1, lat2, lon2) {
     return dist;
   }
 }
+
+function compareInterests(userID, driverList, originLat, originLng) {
+  let bestDriver = driverList[0];
+  driverList.forEach(driver => {
+    if (distanceMatrix(driver.lat, driver.lng, originLat, originLng) < distanceMatrix(bestDriver.lat, bestDriver.lng, originLat, originLng)) {
+      bestDriver = driver;
+    }
+  });
+  return bestDriver
+}
+
+
+
+
+//NOTES FOR MICKEY.
+//1. If it costs money to write to a collection then we dont need to write each request to the DB
+//2. Should we get ask for a response from the rider when we alert then that they have a driver? or should we
+//just end it there?
+//3. Note, we are currently not using isProccessed. and I think isAccepted is used later.
+//4. Alot of the commented out stuff is there for future, I just wrote it out to give me an idea of the workflow
+// Getting the Active Driver list should work though from my testing     
+
+
+// Old routine if in Main
+// admin.firestore().collection("activeDrivers").get()
+//   .then(snapshot => {
+//     let driverList = snapshot.docs.map(doc => {
+//       return doc.data();
+//     });
+//     console.log("Here is a list of current Drivers." + JSON.stringify(driverList));
+//     return driverList;
+//   }).catch(function (error) {
+//     res.status(400).send('Error retrieving Drivers')
+//     return false;
+//   })
